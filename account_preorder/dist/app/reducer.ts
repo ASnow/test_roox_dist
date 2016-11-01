59b5434f1d62a50afbcb723cd082b23dc13837b1
Andrey Bolshov <asnow.dev@gmail.com>
2016-11-01 22:14:31 +0300

    f

--- account_preorder/dist/app/reducer.ts
+++ account_preorder/dist/app/reducer.ts
@@ -1,1 +1,20 @@
-export default (state: any, action: any) => { return state; };
+import { combineReducers } from 'redux';
+import { IDialog, IDialogsState } from './models';
+import { OPEN_DIALOG, CLOSE_DIALOG } from './components/account_preorder.actions';
+
+
+function  dialogsReducer(state: IDialogsState = {stack: [], current: null}, action: any) {
+  switch (action.type) {
+    case OPEN_DIALOG:
+      let newCurrent = state.current || action.payload;
+      return Object.assign({}, state, {stack: [action.payload].concat(state.stack), current: newCurrent});
+    case CLOSE_DIALOG:
+      let newStack = state.stack.slice(1);
+      return Object.assign({}, state, {stack: newStack, current: newStack[0]});
+  }
+  return state;
+};
+
+export default combineReducers({
+  dialogs: dialogsReducer
+});
